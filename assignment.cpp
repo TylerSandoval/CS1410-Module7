3f74c9acacc947ad93bfbc7b10c36eaabab6da18
Tyler <tylersandoval@mail.weber.edu>
2017-10-10 12:06:31 -0600

    Initial commit

---
+++ assignment.cpp
@@ -0,0 +1,51 @@
+// File: assignment
+// Created by Tyler Sandoval on 10/10/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+class Counter
+{
+private:
+    unsigned int count;         // count
+
+public:
+    Counter(): count(0)         // constructor
+    {};
+    unsigned int get_count()    // returns count
+    {
+        return count;
+    }
+    /* The operator keyword is used to overload
+     * the operator in hand*/
+    Counter operator ++()          // increment prefix
+    {
+        ++count;
+        Counter temp;          // create temporary class object
+        temp.count = count;    // increment the count of the temp object
+        return temp;           // return the temp object, with now incremented count
+    }
+
+};
+
+// Prototypes
+
+// Main Program Program
+int main(void)
+{
+    Counter c1, c2, c3;
+    cout << "c1 = " << c1.get_count() << endl;
+    cout << "c2 = " << c2.get_count() << endl;
+    ++c1;
+    ++c2;
+    cout << "c1 = " << c1.get_count() << endl;
+    cout << "c2 = " << c2.get_count() << endl;
+
+    c3 = ++c1;
+    cout << "c3 = " << c3.get_count() << endl;
+
+    return 0;
+}
+// Function Definitions
